10df25031a9f27429da8eb194aae8dfdf6e7d5cb
qiyuor2 <48339849+QiYuOr2@users.noreply.github.com>
2025-03-16 20:51:38 +0800

    feat: avatar scroll animate

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import { ParallaxLayer, Parallax, IParallax } from '@react-spring/parallax'
-import { useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { PROFILE } from './constants';
 import { FloatingStep } from './components/FloatingStep';
+import { useThrottledState } from '@react-hookz/web'
 
 interface PageProps {
   offset: number
@@ -31,26 +32,95 @@ function Experience({ offset }: PageProps) {
 export default function App() {
   const parallax = useRef<IParallax>(null!);
 
-  const scroll = (to: number) => {
+  const [currentStep, setCurrentStep] = useThrottledState(0, 500)
+
+  const scrollTo = (to: number) => {
     if (!parallax.current) {
       return
     }
-
     parallax.current.scrollTo(to)
   }
 
+  const avatarRef = useRef<HTMLDivElement>(null)
+  const [basicStyle, setBasicStyle] = useState({ width: 0, height: 0, left: 0, top: 0 })
+  const [avatarStyle, setAvatarStyle] = useState({ width: 0, height: 0, left: 0, top: 0 })
+
+  useEffect(() => {
+    if (avatarRef.current) {
+      const rect = avatarRef.current.getBoundingClientRect();
+      setBasicStyle({
+        width: rect.width,
+        height: rect.height,
+        left: rect.left,
+        top: rect.top,
+      })
+    }
+
+  }, [])
+
+
+  const computedCurrentStep = () => {
+    const containerHeight = parallax.current.container.current.clientHeight
+    const scrollTop = parallax.current.container.current.scrollTop
+
+    setCurrentStep(Math.round(scrollTop / containerHeight))
+  }
+
+  
+  const computedAvatarStyle = () => {
+    const scrollTop = parallax.current.container.current.scrollTop
+    if (avatarRef.current) {
+
+      const target = {
+        width: 50,
+        height: 50,
+        left: 50,
+        top: 50,
+      }
+
+      const progress = Math.min(1, scrollTop / basicStyle.top);
+
+      setAvatarStyle({
+        width: basicStyle.width + progress * (target.width - basicStyle.width),
+        height: basicStyle.height + progress * (target.height - basicStyle.height),
+        left: basicStyle.left + progress * (target.left - basicStyle.left),
+        top: basicStyle.top + progress * (target.top - basicStyle.top)
+      })
+    }
+  }
+
+
+
+  useEffect(() => {
+    parallax.current?.container.current.addEventListener('scroll', computedCurrentStep)
+    parallax.current?.container.current.addEventListener('scroll', computedAvatarStyle)
+
+    return () => {
+      parallax.current?.container.current.removeEventListener('scroll', computedCurrentStep)
+      parallax.current?.container.current.removeEventListener('scroll', computedAvatarStyle)
+    }
+  }, [basicStyle])
+
+
+
   return (
     <>
-      <Parallax ref={parallax} pages={3} style={{ top: '0', left: '0', width: '100%', height: '100%' }}>
-        <ParallaxLayer speed={-0.2} style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', pointerEvents: 'none' }}>
+      <div className='flex items-center justify-center h-screen'>
+        <div
+          ref={avatarRef}
+          className='w-20 h-20 rounded-full shadow-md overflow-hidden'
+          style={avatarStyle.width ? { ...avatarStyle, position: 'fixed', 'zIndex': 100 } : {}}
+        >
+          <img className='w-full h-full ' src={PROFILE.Avatar} alt="avatar" />
+        </div>
+      </div>
 
-          <img src={PROFILE.Avatar} alt="avatar" className='w-20 h-20 rounded-full shadow-md' />
-        </ParallaxLayer>
+      <Parallax ref={parallax} pages={3} style={{ top: '0', left: '0' }}>
         <Bento offset={1} />
         <Experience offset={2} />
       </Parallax >
 
-      <FloatingStep step={3} toNextStep={scroll} />
+      <FloatingStep step={3} currentStep={currentStep} toNextStep={scrollTo} />
     </>
   )
 }
--- src/components/FloatingStep.tsx
+++ src/components/FloatingStep.tsx
@@ -1,23 +1,21 @@
-import { useState } from 'react'
-
 interface Props {
   step: number
-  nextStep: (to: number) => void
+  currentStep: number
+  toNextStep: (to: number) => void
 }
 
-export function FloatingStep({ step, nextStep }: Props) {
-  const [current, setCurrent] = useState(0)
+export function FloatingStep({ step, currentStep, toNextStep }: Props) {
 
   const handleClick = (index: number) => {
-    setCurrent(index)
-    nextStep(index)
+    toNextStep(index)
   }
 
   return (
-    <div className='fixed bottom-5 left-1/2 translate--1/2 m-auto flex items-center gap-2'>
+    <div className='fixed bottom-5 left-1/2 -translate-x-1/2 m-auto flex items-center gap-2'>
       {new Array(step).fill(0).map((_, i) => (
         <div
-          className={`w-8 h-2 shadow rounded-lg cursor-pointer ${current === i ? 'bg-gray-900' : 'bg-white'} transition-colors duration-250 ease-in-out`}
+          key={i}
+          className={`w-8 h-2 shadow rounded-lg cursor-pointer ${currentStep === i ? 'bg-gray-900' : 'bg-white'} transition-colors duration-250 ease-in-out`}
           onClick={() => handleClick(i)}
         ></div>)
       )}
